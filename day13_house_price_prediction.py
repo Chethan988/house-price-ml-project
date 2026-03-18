3a2d745a5e305b74d8c43f28abd8800e89922a2f
Chethan FK <118584222+Chethan988@users.noreply.github.com>
2026-03-18 16:00:26 +0530

    Refactor comments and improve code readability

--- day13_house_price_prediction.py
+++ day13_house_price_prediction.py
@@ -1,6 +1,4 @@
-# =========================
-# 1. IMPORT LIBRARIES
-# =========================
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -14,9 +12,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 
-# =========================
-# 2. LOAD DATASET
-# =========================
+
+#LOAD DATASET
+
 data = fetch_california_housing()
 
 df = pd.DataFrame(data.data, columns=data.feature_names)
@@ -27,10 +25,7 @@ print(df.info())
 print(df.describe())
 print(df.isnull().sum())
 
-
-# =========================
-# 3. EDA (Visualization)
-# =========================
+# EDA (Visualization)
 sns.histplot(df["price"], bins=30)
 plt.title("Price Distribution")
 plt.show()
@@ -41,22 +36,15 @@ plt.figure(figsize=(10, 8))
 sns.heatmap(corr, annot=True, cmap="coolwarm")
 plt.title("Correlation Matrix")
 plt.show()
-
-
-# =========================
-# 4. SPLIT DATA
-# =========================
+# SPLIT DATA
 X = df.drop("price", axis=1)
 y = df["price"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+# LINEAR REGRESSION (PIPELINE)
 
-
-# =========================
-# 5. LINEAR REGRESSION (PIPELINE)
-# =========================
 pipeline_lr = Pipeline([
     ("scaler", StandardScaler()),
     ("model", LinearRegression())
@@ -69,10 +57,7 @@ print("\n--- Linear Regression (Pipeline) ---")
 print("MSE:", mean_squared_error(y_test, pred_lr))
 print("R2:", r2_score(y_test, pred_lr))
 
-
-# =========================
 # 6. RANDOM FOREST MODEL
-# =========================
 rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
 
 rf_model.fit(X_train, y_train)
@@ -82,18 +67,12 @@ print("\n--- Random Forest ---")
 print("MSE:", mean_squared_error(y_test, pred_rf))
 print("R2:", r2_score(y_test, pred_rf))
 
-
-# =========================
 # 7. MODEL COMPARISON
-# =========================
 print("\n----- MODEL COMPARISON -----")
 print("Linear Regression R2:", r2_score(y_test, pred_lr))
 print("Random Forest R2:", r2_score(y_test, pred_rf))
 
-
-# =========================
-# 8. FEATURE IMPORTANCE (Random Forest)
-# =========================
+#  FEATURE IMPORTANCE (Random Forest)
 importance = pd.DataFrame({
     "Feature": X.columns,
     "Importance": rf_model.feature_importances_
@@ -102,10 +81,8 @@ importance = pd.DataFrame({
 print("\n--- Feature Importance ---")
 print(importance)
 
-
-# =========================
 # 9. COEFFICIENTS (Linear Regression)
-# =========================
+
 coefficients = pd.DataFrame({
     "Feature": X.columns,
     "Coefficient": pipeline_lr.named_steps["model"].coef_
